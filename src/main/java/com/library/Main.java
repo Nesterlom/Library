235e1cac5bb4fe625f2d9c92b193f4084fabb436
Nesterlom <nesterlobr@gmail.com>
2023-01-12 11:28:53 +0300

    Refactoring classes. Add UserRepository and BookRepository instead of DBWorker. Delete DBWorker.

--- src/main/java/com/library/Main.java
+++ src/main/java/com/library/Main.java
@@ -1,165 +1,132 @@
 package com.library;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
     //In main file we chose actions and give commands to DBWorker, which can communicate with database.
     private static final Inputer inputer = Inputer.getInstance();
+    private static final Scanner scan = new Scanner(System.in);
+    private static final Printer printer = Printer.getInstance();
+
+    private static final UserRepository userRepository = UserRepository.getInstance();
+    private static final BookRepository bookRepository = BookRepository.getInstance();
+
+    private static String name;
+    private static String password;
+    private static int id = -1;
+    private static boolean isAuth = false;
+    private static boolean isProgramOn = true;
+
 
     public static void main(String[] args) {
         try {
-            boolean isProgramOn = true;
-            boolean isEntered = false;
-            boolean isAuth = false;
-            String name = ""; // name of user
-            String password = "";// users password
-            int id = -1;
-            Scanner scan = new Scanner(System.in);
-            DBWorker worker = DBWorker.getInstance();
-            Printer printer = Printer.getInstance();
-
             System.out.println("___Welcome to database-oriented library___");
 
             while (isProgramOn) {
                 while (!isAuth) {
-                    System.out.print("""
-                            Choose action:
-                            1.Enter account.
-                            2.End program.
-                            3.Create new account.
-                            """);
-                    int action = inputer.input();
-                    switch (action) {
-                        case 1 -> {
-                            System.out.println("Please enter your username:");
-                            name = scan.next();
-                            System.out.println("Please enter your password:");
-                            password = scan.next();
-
-                            if ((id = worker.auth(name, password)) > 0) {
-                                System.out.println("You have entered account successfully.");
-                                isAuth = true;
-                                isEntered = true;
-                            } else {
-                                System.out.println("Name or password are wrong. Please try once again.");
-                                name = "";
-                                password = "";
-                            }
-                        }
-                        case 2 -> {
-                            System.out.println("See you again!");
-                            isAuth = true;
-                            isProgramOn = false;
-                        }
-                        case 3 -> {
-                            System.out.println("Enter your name:");
-                            name = scan.next();
-                            System.out.println("Enter your password:");
-                            password = scan.next();
-                            System.out.println("Please repeat your password:");
-                            String password2 = scan.next();
-
-                            if (password.equals(password2)) {
-                                if (worker.createNewAccount(name, password)) {
-                                    System.out.println("New account was added");
-                                } else {
-                                    System.out.println("User with such name already exists or something went wrong");
-                                }
-                            } else {
-                                System.out.println("Your passwords has not matched, please try again.");
-                            }
-                        }
-                        default -> {
-                            System.out.println("Bad input, please try again.");
-                        }
-                    }
+                    printer.authChoseBar();
+                    authCaseManager();
                 }
-
-                while (isEntered) {
-                    System.out.print("""
-                            Choose action:
-                            0.Show saved books.
-                            1.Show books from database.
-                            2.Find book.
-                            3.Save book.
-                            4.Delete book.
-                            5.Exit from account.
-                            6.Change password.
-                            7.End program.
-                            """);
-                    int action = inputer.input();
-
-                    switch (action) {
-                        case 0 -> {
-                            printer.showSavedBooks(id);
-                        }
-                        case 1 -> {
-                            printer.showBooks();
-                        }
-                        case 2 -> {
-                            System.out.print("""
-                                    Chose method of finding a book:
-                                    1.By book name.
-                                    2.By author.
-                                    3.By year.
-                                    """);
-                            worker.findBook();
-                        }
-                        case 3 -> {
-                            System.out.println("Enter id of a book that you want to add:\n" +
-                                    "(if you dont know the id you can type '0' to close this operation and check id in the 'Find book'.)");
-                            if (worker.saveBook(id)) {
-                                System.out.println("You have saved book.");
-                            } else {
-                                System.out.println("Something went wrong or you exited.");
-                            }
-                        }
-                        case 4 -> {
-                            System.out.println("Enter id of a book that you want to delete or press '0' to exit:");
-                            if (worker.deleteBook(id)) {
-                                System.out.println("You have delete book.");
-                            } else {
-                                System.out.println("Something went wrong or you exited.");
-                            }
-                        }
-                        case 5 -> {
-                            System.out.println("Disconnect from account ");
-                            isEntered = false;
-                            isAuth = false;
-                            name = "";
-                            password = "";
-                        }
-                        case 6 -> {
-                            System.out.println("Enter your old password: ");
-                            String oldPassword = scan.next();
-                            if (oldPassword.equals(password)) {
-                                System.out.println("Enter your new password: ");
-                                String newPassword = scan.next();
-                                System.out.println("Repeat your new password");
-                                String newPassword2 = scan.next();
-
-                                if (newPassword.equals(newPassword2)) {
-                                    worker.setNewPassword(name, newPassword);
-                                    System.out.println("Password was changed.");
-                                }
-                            }
-                        }
-                        case 7 -> {
-                            System.out.println("See you again!");
-                            isEntered = false;
-                            isProgramOn = false;
-                        }
-                        default -> {
-                            System.out.println("Bad input, please try again.");
-                        }
-                    }
+                while (isAuth && id > 0) {
+                    printer.mainChoseBar();
+                    mainCaseManager();
                 }
             }
         } catch (SQLException e) {
             System.out.println("Error in connection to data base.");
+        }
+    }
+
+    private static void authCaseManager() throws SQLException {
+        switch (inputer.input()) {
+            case 1 -> {
+                System.out.println("Please enter your username:");
+                name = scan.next();
+                System.out.println("Please enter your password:");
+                password = scan.next();
+
+                if ((id = userRepository.auth(name, password)) > 0) {
+                    System.out.println("You have entered account successfully.");
+                    isAuth = true;
+                } else {
+                    System.out.println("Name or password are wrong. Please try once again.");
+                }
+            }
+            case 2 -> {
+                System.out.println("See you again!");
+                isAuth = true;
+                isProgramOn = false;
+            }
+            case 3 -> {
+                System.out.println("Enter your name:");
+                name = scan.next();
+                System.out.println("Enter your password:");
+                password = scan.next();
+                System.out.println("Please repeat your password:");
+                String password2 = scan.next();
+
+                if (password.equals(password2)) {
+                    if (userRepository.createNewAccount(name, password)) {
+                        System.out.println("New account was added");
+                    } else {
+                        System.out.println("User with such name already exists or something went wrong");
+                    }
+                } else {
+                    System.out.println("Your passwords has not matched, please try again.");
+                }
+            }
+            default -> System.out.println("Bad input, please try again.");
+        }
+    }
+    private static void mainCaseManager() throws SQLException {
+        switch (inputer.input()) {
+            case 0 -> {
+                System.out.println("Enter page number: ");
+                printer.showSavedBooks(inputer.input(), id);
+            }
+            case 1 -> {
+                System.out.println("Enter page number: ");
+                printer.showBooks(inputer.input());
+            }
+            case 2 -> {
+                printer.findBookChooseBar();
+                bookRepository.findBook();
+            }
+            case 3 -> {
+                System.out.println("Enter id of a book that you want to add:\n" +
+                        "(if you dont know the id you can type '0' to close this operation and check id in the 'Find book'.)");
+                printer.printBookSaved(bookRepository.saveBook(id));
+            }
+            case 4 -> {
+                System.out.println("Enter id of a book that you want to delete or press '0' to exit:");
+                printer.printBookDeleted(bookRepository.deleteBook(id));
+            }
+            case 5 -> {
+                System.out.println("Disconnect from account ");
+                id = -1;
+                isAuth = false;
+            }
+            case 6 -> {
+                System.out.println("Enter your old password: ");
+                if (scan.next().equals(password)) {
+                    System.out.println("Enter your new password: ");
+                    String newPassword = scan.next();
+                    System.out.println("Repeat your new password");
+                    String newPassword2 = scan.next();
+
+                    if (newPassword.equals(newPassword2)) {
+                        userRepository.setNewPassword(id, newPassword);
+                        System.out.println("Password was changed.");
+                    }
+                }
+            }
+            case 7 -> {
+                System.out.println("See you again!");
+                isAuth = false;
+                isProgramOn = false;
+            }
+            default -> System.out.println("Bad input, please try again.");
         }
     }
 }
